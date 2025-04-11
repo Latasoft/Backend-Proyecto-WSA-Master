db6171cb91bf482c242dab2fa07de497bf2751cf
Mattiias56 <mat.molinas@duocuc.cl>
2025-04-10 22:08:12 -0400

    cambiando a mongo atlas

--- src/config/mongoConfig.js
+++ src/config/mongoConfig.js
@@ -3,7 +3,7 @@ import { MONGO_URI } from '../config/config.js';
 
 export const connectMongoDB = async () => {
   try {
-    const mongoUri =  'mongodb://localhost:27017/wsa_bd';
+    const mongoUri =  MONGO_URI;
     await mongoose.connect(mongoUri);
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
