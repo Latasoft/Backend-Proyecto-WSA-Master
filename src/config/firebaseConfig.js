857c838df053e44da8e1950c30d2a30b93092720
Mattiias56 <mat.molinas@duocuc.cl>
2025-04-10 21:43:07 -0400

    agregando configuracion cloud

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Ruta absoluta al archivo de credenciales
-const serviceAccountPath = "./firebase-key.json";
+const serviceAccountPath = "/etc/secrets/firebase-key.json";
 
 // Leer el archivo JSON de credenciales
 const serviceAccount = JSON.parse(
