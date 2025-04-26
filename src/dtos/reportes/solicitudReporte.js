43129f85fa8be119fbb71140dd233dbd0ee06a4a
Mattiias56 <mat.molinas@duocuc.cl>
2025-04-26 13:29:21 -0400

    Arreglando dto

--- src/dtos/reportes/solicitudReporte.js
+++ src/dtos/reportes/solicitudReporte.js
@@ -3,9 +3,20 @@ import {z} from 'zod'
 const estadoSolicitud = ['PENDIENTE','ACEPTADO','RECHAZADO']
 const RegistroSolicitudReporteSchema = z.object({
     id_solicitante: z.string().min(1,'el id del solicitante es requerido'),
-    fecha_solicitud: z.date().default(new Date()),
-    rango_fecha_inicio: z.date(),
-    rango_fecha_termino: z.date(),
+    rango_fecha_inicio: z.preprocess(
+        (arg) => {
+          if (typeof arg === "string" || arg instanceof Date) return new Date(arg);
+          return arg;
+        },
+        z.date()
+      ),
+    rango_fecha_termino: z.preprocess(
+        (arg) => {
+          if (typeof arg === "string" || arg instanceof Date) return new Date(arg);
+          return arg;
+        },
+        z.date()
+      ),
     estado_solicutud:z.enum(estadoSolicitud).default('PENDIENTE'),
     documento_reporte:z.string().default(null).optional()
 })
